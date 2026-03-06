9a58e755db5e24d27172630194959fe26398509c
mriccidesigns <mriccidesigns@gmail.com>
2026-03-06 13:28:48 -0800

    Fix white screen on custom domain: update Router basename from /DesignPortfolio/ to /

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import ScrollToTop from './components/ScrollToTop';
 
 function App() {
     return (
-        <Router basename="/DesignPortfolio/">
+        <Router basename="/">
             <ScrollToTop />
             <Routes>
                 <Route path="/" element={<Layout />}>
